a3b677372ce1cb81eb52da9be031f16a2fd2d876
FMCalisto <francisco_mcalisto@hotmail.com>
2015-08-29 17:09:16 +0100

    [CHANGE] Final Interrogator View

--- js/questions.js
+++ js/questions.js
@@ -1,28 +1,81 @@
-$(document).ready(function(){
-    var next = 1;
-    $(".add-more").click(function(e){
-        e.preventDefault();
-        var addto = "#field" + next;
-        var addRemove = "#field" + (next);
-        next = next + 1;
-        var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
-        var newInput = $(newIn);
-        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
-        var removeButton = $(removeBtn);
-        $(addto).after(newInput);
-        $(addRemove).after(removeButton);
-        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
-        $("#count").val(next);  
-        
-            $('.remove-me').click(function(e){
-                e.preventDefault();
-                var fieldNum = this.id.charAt(this.id.length-1);
-                var fieldID = "#field" + fieldNum;
-                $(this).remove();
-                $(fieldID).remove();
-            });
+/* 
+    Text fields 
+*/
+$(function(){
+    $(document).on('focus', 'div.form-group-options div.input-group-option:last-child input', function(){
+        var sInputGroupHtml = $(this).parent().html();
+        var sInputGroupClasses = $(this).parent().attr('class');
+        $(this).parent().parent().append('<div class="'+sInputGroupClasses+'">'+sInputGroupHtml+'</div>');
     });
     
+    $(document).on('click', 'div.form-group-options .input-group-addon-remove', function(){
+        $(this).parent().remove();
+    });
+});
 
+/* 
+    Selects 
+*/
+$(function(){
+        
+    var values = new Array();
     
+    $(document).on('change', '.form-group-multiple-selects .input-group-multiple-select:last-child select', function(){
+        
+        var selectsLength = $('.form-group-multiple-selects .input-group-multiple-select select').length;
+        var optionsLength = ($(this).find('option').length)-1;
+        
+        if(selectsLength < optionsLength){
+            var sInputGroupHtml = $(this).parent().html();
+            var sInputGroupClasses = $(this).parent().attr('class');
+            $(this).parent().parent().append('<div class="'+sInputGroupClasses+'">'+sInputGroupHtml+'</div>');  
+        }
+        
+        updateValues();
+        
+    });
+    
+    $(document).on('change', '.form-group-multiple-selects .input-group-multiple-select:not(:last-child) select', function(){
+        
+        updateValues();
+        
+    });
+    
+    $(document).on('click', '.input-group-addon-remove', function(){
+        $(this).parent().remove();
+        updateValues();
+    });
+    
+    function updateValues()
+    {
+        values = new Array();
+        $('.form-group-multiple-selects .input-group-multiple-select select').each(function(){
+            var value = $(this).val();
+            if(value != 0 && value != ""){
+                values.push(value);
+            }
+        });
+        
+        $('.form-group-multiple-selects .input-group-multiple-select select').find('option').each(function(){
+            var optionValue = $(this).val();
+            var selectValue = $(this).parent().val();
+            if(in_array(optionValue,values)!= -1 && selectValue != optionValue)
+            {
+                $(this).attr('disabled', 'disabled');
+            }
+            else
+            {
+                $(this).removeAttr('disabled');
+            }
+        });
+    }
+    
+    function in_array(needle, haystack){
+        var found = 0;
+        for (var i=0, length=haystack.length;i<length;i++) {
+            if (haystack[i] == needle) return i;
+            found++;
+        }
+        return -1;
+    }
 });
