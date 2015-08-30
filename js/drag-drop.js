e5b5d11657bec9385cb1bc48da63171d4a18f594
FMCalisto <francisco_mcalisto@hotmail.com>
2015-08-30 17:37:30 +0100

    [CHANGE] Prototype HFP5

--- js/drag-drop.js
+++ js/drag-drop.js
@@ -1,40 +1,27 @@
-/* The dragging code for '.draggable' from the demo above
- * applies to this demo as well so it doesn't have to be repeated. */
+function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+}
 
-// enable draggables to be dropped into this
-interact('.dropzone').dropzone({
-  // only accept elements matching this CSS selector
-  accept: '#yes-drop',
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.75,
+function dragEnter(event) {
+    if ( event.target.className == "droptarget" ) {
+        document.getElementById("demo").innerHTML = "Entered the dropzone";
+        event.target.style.border = "3px dotted red";
+    }
+}
 
-  // listen for drop related events:
+function dragLeave(event) {
+    if ( event.target.className == "droptarget" ) {
+        document.getElementById("demo").innerHTML = "Left the dropzone";
+        event.target.style.border = "";
+    }
+}
 
-  ondropactivate: function (event) {
-    // add active dropzone feedback
-    event.target.classList.add('drop-active');
-  },
-  ondragenter: function (event) {
-    var draggableElement = event.relatedTarget,
-        dropzoneElement = event.target;
+function allowDrop(event) {
+    event.preventDefault();
+}
 
-    // feedback the possibility of a drop
-    dropzoneElement.classList.add('drop-target');
-    draggableElement.classList.add('can-drop');
-    draggableElement.textContent = 'Dragged in';
-  },
-  ondragleave: function (event) {
-    // remove the drop feedback style
-    event.target.classList.remove('drop-target');
-    event.relatedTarget.classList.remove('can-drop');
-    event.relatedTarget.textContent = 'Dragged out';
-  },
-  ondrop: function (event) {
-    event.relatedTarget.textContent = 'Dropped';
-  },
-  ondropdeactivate: function (event) {
-    // remove active dropzone feedback
-    event.target.classList.remove('drop-active');
-    event.target.classList.remove('drop-target');
-  }
-});
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+}
