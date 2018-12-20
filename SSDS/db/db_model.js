5a13a9ef3deef8db56a93a09c122d6861d1dda60
FMCalisto <FMCalisto@users.noreply.github.com>
2018-12-20 19:02:20 +0000

    [CREATE] New Source

--- SSDS/db/db_model.js
+++
@@ -1,8 +0,0 @@
-// grab the things we need
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/dbteste");
-var Answer = mongoose.model('Answer',{ans:Array}); //can be array
-
-
-// make this available to our users in our Node applications
-module.exports = Answer;
